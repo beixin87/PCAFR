a520d2e3db81bfeccf325d4f54f0fc5e53a882db
Jonas Yao <jyao@splunk.com>
2015-11-17 13:21:34 -0800

    minor

--- src/dataSummary.py
+++ src/dataSummary.py
@@ -1,10 +1,10 @@
 __author__ = 'aradon'
 
-# import numpy as np
 import pandas as pd
+import requests
 
 
-Location = '/Users/aradon/PycharmProjects/Sample/data/event_data.csv' # add your file path of csv file
+Location = '../data/event_data.csv' # add your file path of csv file
 df = pd.read_csv(Location)
 
 def pieChartEventNumberPerLocation():
@@ -25,6 +25,33 @@ def pieChartEventNumberPerMonth():
         else:
             monthDict[key] = 1
     return monthDict # search through the dictionary to populate the pie chart
+
+def data_upload():
+    eventPerLocation = pieChartEventNumberPerLocation()
+    monthDict = pieChartEventNumberPerMonth()
+    try:
+        for location in eventPerLocation:
+            print location
+            print eventPerLocation[location]
+            data = '{"geo_location": "' + str(location) + '", "event_number": "' + str(eventPerLocation[location]) + '", "volunteer_number": "' + "0" + '", "month": "' + "test" + '", "month_event_number": "' + "0" + '", "month_volunteer_number": "' + "0" + '"}'
+            headers = {'Content-Type': 'application/json'}
+            response = requests.post('http://localhost:8000/api/v1/stat/', data=data, headers=headers)
+            print response
+        for month in monthDict:
+            print month
+            print monthDict[month]
+            data = '{"geo_location": "' + "test" + '", "event_number": "' + "0" + '", "volunteer_number": "' + "0" + '", "month": "' + str(month) + '", "month_event_number": "' + str(monthDict[month]) + '", "month_volunteer_number": "' + "0" + '"}'
+            headers = {'Content-Type': 'application/json'}
+            response = requests.post('http://localhost:8000/api/v1/stat/', data=data, headers=headers)
+            print response
+    except:
+        raise
+
+if __name__ == "__main__":
+    data_upload()
+
+
+
 
 
 
